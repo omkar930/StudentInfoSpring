c0f3f5a79cd2c2fa684921b5c5a10e7d626ca5b2
omkar930 <93o69644o9@gmail.com>
2024-02-26 19:51:05 +0530

    created the student data model

---
+++ src/main/java/com/omkar/models/Student.java
@@ -0,0 +1,16 @@
+package com.omkar.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String adhar;
+    private String university;
+}
